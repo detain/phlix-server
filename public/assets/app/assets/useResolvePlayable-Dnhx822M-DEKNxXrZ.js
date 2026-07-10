68ae481528a8199a41fbee5537d9c1b92e5c94fa
Joe Huss <detain@interserver.net>
2026-07-10 14:39:33 -0400

    chore: repin @phlix/ui to v0.77.0 (player audio/subtitle track selection + Original quality) (#512)

---
+++ public/assets/app/assets/useResolvePlayable-Dnhx822M-DEKNxXrZ.js
@@ -0,0 +1,2 @@
+import{r as e}from"./episode-order-BMC9lH44-Da84bkks.js";import{t}from"./useSeriesSeasons-Dga2EzCU-CmL07P9j.js";function n(e,t){return(e[t]??0)>0}function r(t,r){let i=e(t.flatMap(e=>e.episodes));return i.length===0?null:i.find(e=>n(r,e.id))??i[0]}async function i(e,n,i,a,o){return i.type===`series`||i.type===`season`?r(await t(e,n,i.id,o),a):i}export{r as n,i as t};
+//# sourceMappingURL=useResolvePlayable-Dnhx822M-DEKNxXrZ.js.map
